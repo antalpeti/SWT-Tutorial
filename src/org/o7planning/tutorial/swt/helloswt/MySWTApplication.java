7b9545852a5a56cf23a0d41bc69aab766690974a
Peti <antalpeti@users.noreply.github.com>
2015-05-19 20:41:29 +0200

    Examined the behavior of the radio button. One radio button can be selected on a composite.

--- src/org/o7planning/tutorial/swt/helloswt/MySWTApplication.java
+++ src/org/o7planning/tutorial/swt/helloswt/MySWTApplication.java
@@ -2,10 +2,12 @@ package org.o7planning.tutorial.swt.helloswt;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 
 public class MySWTApplication {
@@ -57,11 +59,29 @@ public class MySWTApplication {
     btnMyButton.setText("My Button");
 
     Button btnRadioButton = new Button(composite, SWT.RADIO);
-    btnRadioButton.setText("Radio Button");
+    btnRadioButton.setText("Radio Up");
 
     Button btnCheckButton = new Button(composite, SWT.CHECK);
     btnCheckButton.setText("Check Button");
+    new Label(composite, SWT.NONE);
 
+    Button btnRadioButton_1 = new Button(composite, SWT.RADIO);
+    btnRadioButton_1.setText("Radio Down");
+    new Label(composite, SWT.NONE);
+
+    Composite composite_1 = new Composite(composite, SWT.NONE);
+    composite_1.setLayout(new GridLayout(3, false));
+    composite_1.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false, 3, 1));
+
+    Button btnRadioButton_2 = new Button(composite_1, SWT.RADIO);
+    btnRadioButton_2.setBounds(0, 0, 90, 16);
+    btnRadioButton_2.setText("Radio Left");
+
+    Button btnRadioButton_3 = new Button(composite_1, SWT.RADIO);
+    btnRadioButton_3.setBounds(0, 0, 90, 16);
+    btnRadioButton_3.setText("Radio Center");
+
+    Button btnRadioButton_4 = new Button(composite_1, SWT.RADIO);
+    btnRadioButton_4.setText("Radio Right");
   }
-
 }
